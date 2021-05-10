cafc745178f694da340ff309c6b5ccf4df0b910a
Alex Fourmy <alexfourmy@gmail.com>
2021-05-10 11:09:56 +0200

    completed rolling dice functionality

--- script.js
+++ script.js
@@ -32,10 +32,13 @@ document.addEventListener('keydown', function(e) {
 })
 
 // Selecting elements
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.getElementById('score--0')
 const score1El = document.getElementById('score--1')
 const current0El = document.getElementById('current--0')
 const current1El = document.getElementById('current--1')
+
 const diceEl = document.querySelector('.dice')
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
@@ -46,7 +49,15 @@ score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
 
+const scores = [0, 0]
 let currentScore = 0
+let activePlayer = Math.trunc(Math.random() * 2)
+
+if(activePlayer === 0) {
+  player0El.classList.add('player--active')
+} else {
+  player1El.classList.add('player--active')
+}
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function() {
@@ -58,8 +69,23 @@ btnRoll.addEventListener('click', function() {
   // 3. Check for rolled 1, if true, switch to next player
   if(dice !== 1) {
     currentScore += dice
-    current0El.textContent = currentScore
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore
   } else {
-
+    document.getElementById(`current--${activePlayer}`).textContent = 0
+    currentScore = 0
+    activePlayer = activePlayer === 0 ? 1 : 0
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
   }
+})
+
+// Holding score functionality
+btnHold.addEventListener('click', function() {
+  // 1. Add current score to active player's score
+
+  // 2. Check if player's score is >= 100
+  // Finish the game
+  
+  // Switch to the next player
+  
 })
